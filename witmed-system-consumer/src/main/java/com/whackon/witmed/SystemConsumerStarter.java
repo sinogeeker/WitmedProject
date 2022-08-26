c7387c1b6be9c3c6c14ac637bf36ae31458dec5c
Arthur <sinogeeker@163.com>
2022-08-26 11:45:34 +0800

    创建系统功能 Consumer 依赖项目：witmed-system-consumer

---
+++ witmed-system-consumer/src/main/java/com/whackon/witmed/SystemConsumerStarter.java
@@ -0,0 +1,21 @@
+package com.whackon.witmed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * <b>智慧医疗信息平台 - 系统功能 - Consumer 启动类</b>
+ *
+ * @author Arthur
+ * @date 2022/8/25
+ */
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication
+public class SystemConsumerStarter {
+	public static void main(String[] args) {
+		SpringApplication.run(SystemConsumerStarter.class, args);
+	}
+}
